587e1b53a05adcac25ab5d1969af412aada1200d
Ethan_Cavallo <ethan.cavallo@epfedu.fr>
2024-09-17 16:16:38 +0200

    Exo1 TP0

---
+++ Exo1/src/exo1/Exo1.java
@@ -0,0 +1,29 @@
+/*
+ * *17 septembre 2024
+ * , ethan cavallo
+ * , TP0 exo 1
+*/
+package exo1;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author ethan
+ */
+public class Exo1 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        System.out.println("Bonjour");
+        String prenom;
+        Scanner sc;
+        sc = new Scanner(System.in);
+        System.out.println("Bonjour, quel est votre prenom ?");
+        prenom = sc.nextLine();
+        System.out.println("Au revoir" );
+    }
+    
+}
