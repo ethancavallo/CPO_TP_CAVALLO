c41d6adc9abf4d1d4893f761b457268ac31b9839
Ethan_Cavallo <ethan.cavallo@epfedu.fr>
2024-10-16 12:28:30 +0200

    test affichage

---
+++ testaffichage/src/testaffichage/Testaffichage.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package testaffichage;
+
+/**
+ *
+ * @author ethan
+ */
+public class Testaffichage {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
